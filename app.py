83abce9acb80532fc5aa600de44dcbf53e21ea38
KingaSzmyd <kingaszmyd@gmail.com>
2021-09-27 19:01:01 +0000

    Adding contact form to contact.html

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash 
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -24,8 +27,10 @@ def about():
     return render_template("about.html", page_title="About Us", company=data)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have recived your message!".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact Us")
 
 
